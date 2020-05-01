58610f018488be0ceace59cf787942e62cfb2c53
mounika120 <mounikapolisetti97@gmail.com>
2020-05-01 18:26:05 +0200

    First commit

--- main.py
+++ main.py
@@ -1,83 +1,34 @@
 from flask import Flask, request
 from fractions import Fraction
 from decimal import Decimal
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return 'Give the values for A and B in the URL form'
-	
+    return 'add num1+num2'
+
+
 @app.route('/add')
 def Addition():
-    try:
-        num1 = request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError:
-        num1 = 'None'
-    try:
-        num2 = request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError:
-        num2 = 'None'
-    if num1=='None' or num2 == 'None':
-        return'Error: please enter valid inputs'
+    num1=request.args.get('A',default = 0, type = Fraction)
+    num2=request.args.get('B',default = 0, type = Fraction)
+    num3= Fraction(num1)
+    num4= Fraction(num2)
+    X=num3+num4
+   
+    Z = str(X).split('/')
+    if len(Z) == 2:
+        num5 = int(Z[0])/int(Z[1])
+        num6= str(num5).split('.')
+        if num6[1] == '0':
+            return '$ \n'% num6[0]
+        else:
+            return '$ \n' % num5
     else:
-        num3= Fraction(num1)
-        num4= Fraction(num2)
-        X = num3+num4
-        return str(round(float(X),6))
+        result1=str(X).split('.')
+        return '$ \n'% result1[0]
 
-@app.route('/sub')
-def Substraction():
-    try:
-        num1 = request.args.get('A' , default = 0, type = Fraction)
-    except ZeroDivisionError:
-        num1 = 'None'
-    try:
-        num2 = request.args.get('B',default =0, type = Fraction)
-    except ZeroDivisionError:
-        num2 = 'None'
-    if num1 == 'None' or num2 == 'None':
-        return'Error: please enter valid inputs'
-    else:
-        num3= Fraction(arg1)
-        num4 = Fraction(arg2)
-        X= num3-num4
-        return str(round(float(X),6))
-        
-@app.route('/mul')
-def Multiplication():
-    try:
-        num1 = request.args.get('A' , default=0, type = Fraction)
-    except ZeroDivisionError:
-        num1 = 'None'
-    try:
-        num2 = request.args.get('B' , default=0, type = Fraction)
-    except ZeroDivisionError:
-        num2 = 'None'
-    if num1 == 'None' or num2 == 'None':
-        return 'Error:please enter valid inputs'
-    else:
-        num3= Fraction(arg1)
-        num4= Fraction(arg2)
-        X= num3*num4
-        return str(round(float(X),6))
-       
-@app.route('/div')
-def Division():
-    try:
-        num1 = request.args.get('A' , default=0, type = Fraction)
-    except ZeroDivisionError:
-        num1= 'None'
-    try:
-        num2 = request.args.get('B' , default=0, type = Fraction)
-    except ZeroDivisionError:
-        num2 = 'None'
-    if num3 == 'None' or num4 == 'None':
-        return 'Error:please enter valid inputs'
-    else:
-        num3= Fraction(arg1)
-        num4= Fraction(arg2)
-        X = num3/num4
-        return str(round(float(X),6))
-    
-    
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
