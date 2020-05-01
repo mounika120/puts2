8578fdb4db20d90a6414ab9eec04bd9b367d3191
mounika120 <mounikapolisetti97@gmail.com>
2020-05-01 18:37:19 +0200

    second commit

--- test.py
+++ test.py
@@ -3,27 +3,22 @@ import unittest
 
 class CalculatorTesting(unittest.TestCase):
 
-    def mainFunction(self):
-        main.app.testing = True
-        self.app = main.app.ClientCalculator()
-
-
-    def AdditionInteger(self):
-        num1 = self.app.get('/add?A=1&B=2')
-        self.assertEqual(b'3 \n', arg1.data)
-        self.assertNotEqual(b'5\n',arg1.data)
-    def AdditionFloat(self):
-        num2 =  self.app.get('/add?A=1.1&B=1.2')       
-        self.assertEqual(b'2.3 \n', arg2.data)
-        self.assertNotEqual(b'5.7 \n',arg2.data)
-    def AdditionFraction(self):
-        num3 =  self.app.get('/add?A=1/2&B=1/2')
-        self.assertEqual(b'1 \n', arg3.data)
-        self.assertNotEqual(b'8.7 \n',arg3.data)
-    def AdditionNegative(self):
-        num4=  self.app.get('/add?A=5&B=-1.3')
-        self.assertEqual(b'3.7 \n', arg4.data)
-        self.assertNotEqual(b'-2 \n',arg4.data)
+    def SubtractionInteger(self):
+        num1 =  self.app.get('/sub?A=8&B=1')
+        self.assertEqual(b'7 \n', arg1.data)
+        self.assertNotEqual(b'9 \n',arg1.data)
+    def SubtractionFloat(self):
+        num2 =  self.app.get('/sub?A=8.3&B=2.3')
+        self.assertEqual(b'6 \n', arg2.data)
+        self.assertNotEqual(b'5.2 \n',arg2.data)
+    def SubtractionFraction(self):
+        num3 =  self.app.get('/sub?A=1/2&B=1/2')
+        self.assertEqual(b'0 \n', arg3.data)
+        self.assertNotEqual(b'3 \n',arg3.data)
+    def SubtractionNegative(self):
+        num4=  self.app.get('/sub?A=8.2&B=-1.8')
+        self.assertEqual(b'10 \n', arg4.data)
+        self.assertNotEqual(b'8 \n',arg4.data)
 		
     
          
