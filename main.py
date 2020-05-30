4154b11b9e5c674f227fab093be518795689df0e
mounika120 <mounikapolisetti97@gmail.com>
2020-05-30 20:25:04 +0200

    first commit

--- main.py
+++ main.py
@@ -1,34 +1,96 @@
 from flask import Flask, request
 from fractions import Fraction
 from decimal import Decimal
-
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return 'div num1/num2'
+    return 'write values of A and B \n write in the format\n'
 
+@app.route('/add')
+def addition():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+    
+    
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        num1 = Fraction(value1)
+        num2= Fraction(value2)
+        result= num1+num2
+        return str(round(float(result),3))
+
+@app.route('/sub')
+def subtraction():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+   
+        
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        num1 = Fraction(value1)
+        num2= Fraction(value2)
+        result= num1-num2
+        return(str(round(float(result),3)))
+
+
+@app.route('/mul')
+def multiplication():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+    
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        num1 = Fraction(value1)
+        num2= Fraction(value2)
+        result= num1*num2
+        return(str(round(float(result),3)))
 
 @app.route('/div')
-def Division():
-    num1=request.args.get('A',default = 0, type = Fraction)
-    num2=request.args.get('B',default = 0, type = Fraction)
-    num3= Fraction(num1)
-    num4= Fraction(num2)
-    X=num3/num4
+def division():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
    
-    Z = str(X).split('/')
-    if len(Z) == 2:
-        num5 = int(Z[0])/int(Z[1])
-        num6= str(num5).split('.')
-        if num6[1] == '0':
-            return '$ \n'% num6[0]
-        else:
-            return '$ \n' % num5
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
     else:
-        result1=str(X).split('.')
-        return '$ \n'% result1[0]
-
+        num1 = Fraction(value1)
+        num2= Fraction(value2)
+        try:
+            result= num1/num2
+            return(str(round(float(result),3)))
+        except ZeroDivisionError as error:
+            return 'None'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
